69dc3a416c5d9f55d69413adc9249c6f61a30456
Nate Schneider <nschneider@mhgi.net>
2018-12-19 09:29:52 -0500

    adde ColorKey

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Footer.scss';
+import ColorKey from './ColorKey';
+
 
 export default class Footer extends Component {
     constructor(props) {
@@ -22,6 +24,7 @@ export default class Footer extends Component {
                     <div className="menu-button-bar" />
                     <div className="menu-button-bar" />
                 </div>
+                <ColorKey />
                 <div className="slider">{this.props.slider}</div>
             </nav>
         );
